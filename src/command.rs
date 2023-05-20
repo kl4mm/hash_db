29021ac20338564d17b792ad563ca60a26d3fe16
Subhan <s.ashraf9@protonmail.com>
2023-05-20 20:41:57 +0100

    move command handling to command module

--- src/command.rs
+++ src/command.rs
@@ -1,3 +1,21 @@
+use std::{
+    io::{self, SeekFrom, Write},
+    sync::Arc,
+    time::{SystemTime, UNIX_EPOCH},
+};
+
+use tokio::{
+    fs::OpenOptions,
+    io::{AsyncSeekExt, AsyncWriteExt, BufReader, BufWriter},
+    sync::RwLock,
+};
+
+use crate::{
+    db,
+    entry::Entry,
+    key_dir::{KeyData, KeyDir},
+};
+
 pub enum Command<'a> {
     Insert(&'a str, &'a str),
     Delete(&'a str),
@@ -5,9 +23,9 @@ pub enum Command<'a> {
     None,
 }
 
-impl<'a> Command<'a> {
-    pub fn from_str(s: &'a str) -> Self {
-        let split: Vec<&str> = s.split_whitespace().collect();
+impl<'a> Into<Command<'a>> for &'a str {
+    fn into(self) -> Command<'a> {
+        let split: Vec<&str> = self.split_whitespace().collect();
 
         if split.len() < 2 || split.len() > 3 {
             return Command::None;
@@ -37,5 +55,94 @@ impl<'a> Command<'a> {
             }
             _ => Command::None,
         }
+    }
+}
+
+impl<'a> Command<'a> {
+    pub async fn handle(
+        command: Command<'a>,
+        key_dir: &Arc<RwLock<KeyDir>>,
+        write_to: &mut impl Write,
+    ) -> io::Result<()> {
+        match command {
+            Command::Insert(k, v) => {
+                let (file, position, file_path) = db::open_latest(key_dir).await?;
+
+                // Get current time
+                let time = SystemTime::now()
+                    .duration_since(UNIX_EPOCH)
+                    .expect("Time before UNIX Epoch")
+                    .as_secs();
+
+                // Get entry bytes
+                let entry = Entry::new_bytes(k, v, time);
+
+                // Write the entry
+                let mut writer = BufWriter::new(file);
+                writer.write_all(&entry).await?;
+                writer.flush().await?;
+
+                // Insert the key and offset to index
+                key_dir.write().await.insert(
+                    k.into(),
+                    KeyData {
+                        path: file_path,
+                        value_s: v.len() as u64,
+                        pos: position,
+                        time,
+                    },
+                );
+
+                write_to.write_all(b"OK\n")?;
+                write_to.flush()?;
+            }
+            Command::Delete(k) => {
+                if let Some(key_data) = key_dir.write().await.remove(k.into()) {
+                    let mut file = OpenOptions::new().write(true).open(&key_data.path).await?;
+
+                    // Find start of entry
+                    file.seek(SeekFrom::Start(key_data.pos)).await?;
+
+                    // Write 1 in delete position of entry:
+                    let mut writer = BufWriter::new(file);
+                    writer.write_u8(1).await?;
+                    writer.flush().await?;
+
+                    // Write to stdout
+                    write_to.write(b"OK\n")?;
+                    write_to.flush()?;
+                }
+            }
+            Command::Get(k) => {
+                if let Some(key_data) = key_dir.read().await.get(k) {
+                    let file = OpenOptions::new().read(true).open(&key_data.path).await?;
+
+                    // Find start of entry
+                    let mut reader = BufReader::new(file);
+                    reader.seek(SeekFrom::Start(key_data.pos)).await?;
+
+                    let entry = match Entry::read(&mut reader).await {
+                        Some(e) => e,
+                        None => {
+                            write_to.write_all(b"There was a problem reading the entry\n")?;
+                            write_to.flush()?;
+                            return Ok(());
+                        }
+                    };
+
+                    write_to.write(&entry.key)?;
+                    write_to.write(b" ")?;
+                    write_to.write(&entry.value)?;
+                    write_to.write(b"\n")?;
+                    write_to.flush()?;
+                }
+            }
+            Command::None => {
+                write_to.write_all(b"Invalid Command. Usage:\nINSERT key value\nGET key\n")?;
+                write_to.flush()?;
+            }
+        }
+
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
-use std::io::{
-    self, BufRead, BufReader as SyncBufReader, BufWriter as SyncBufWriter, SeekFrom, Write,
-};
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::io::{self, BufRead, BufReader as SyncBufReader, BufWriter as SyncBufWriter};
 
 use hash_db::command::Command;
 use hash_db::db;
-use hash_db::entry::Entry;
-use hash_db::key_dir::{self, KeyData};
-use tokio::fs::OpenOptions;
-use tokio::io::{AsyncSeekExt, AsyncWriteExt, BufReader, BufWriter};
+use hash_db::key_dir;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -16,91 +10,14 @@ async fn main() -> io::Result<()> {
 
     let mut stdin = SyncBufReader::new(io::stdin());
     let mut stdout = SyncBufWriter::new(io::stdout());
-    let mut stderr = SyncBufWriter::new(io::stderr());
 
     let mut buf = String::new();
     loop {
         stdin.read_line(&mut buf)?;
 
-        match Command::from_str(&buf) {
-            Command::Insert(k, v) => {
-                let (file, position, file_path) = db::open_latest(&key_dir).await?;
-
-                // Get current time
-                let time = SystemTime::now()
-                    .duration_since(UNIX_EPOCH)
-                    .expect("Time before UNIX Epoch")
-                    .as_secs();
-
-                // Get entry bytes
-                let entry = Entry::new_bytes(k, v, time);
-
-                // Write the entry
-                let mut writer = BufWriter::new(file);
-                writer.write_all(&entry).await?;
-                writer.flush().await?;
-
-                // Insert the key and offset to index
-                key_dir.write().await.insert(
-                    k.into(),
-                    KeyData {
-                        path: file_path,
-                        value_s: v.len() as u64,
-                        pos: position,
-                        time,
-                    },
-                );
-
-                stdout.write_all(b"OK\n")?;
-                stdout.flush()?;
-            }
-            Command::Delete(k) => {
-                if let Some(key_data) = key_dir.write().await.remove(k.into()) {
-                    let mut file = OpenOptions::new().write(true).open(&key_data.path).await?;
-
-                    // Find start of entry
-                    file.seek(SeekFrom::Start(key_data.pos)).await?;
-
-                    // Write 1 in delete position of entry:
-                    let mut writer = BufWriter::new(file);
-                    writer.write_u8(1).await?;
-                    writer.flush().await?;
-
-                    // Write to stdout
-                    stdout.write(b"OK\n")?;
-                    stdout.flush()?;
-                }
-            }
-            Command::Get(k) => {
-                if let Some(key_data) = key_dir.read().await.get(k) {
-                    let file = OpenOptions::new().read(true).open(&key_data.path).await?;
-
-                    // Find start of entry
-                    let mut reader = BufReader::new(file);
-                    reader.seek(SeekFrom::Start(key_data.pos)).await?;
-
-                    let entry = match Entry::read(&mut reader).await {
-                        Some(e) => e,
-                        None => {
-                            stderr.write_all(b"There was a problem reading the entry\n")?;
-                            stderr.flush()?;
-                            continue;
-                        }
-                    };
-
-                    // Write to stdout
-                    stdout.write(&entry.key)?;
-                    stdout.write(b" ")?;
-                    stdout.write(&entry.value)?;
-                    stdout.write(b"\n")?;
-                    stdout.flush()?;
-                }
-            }
-            Command::None => {
-                stderr.write_all(b"Invalid Command. Usage:\nINSERT key value\nGET key\n")?;
-                stderr.flush()?;
-            }
-        };
+        if let Err(e) = Command::handle(buf.as_str().into(), &key_dir, &mut stdout).await {
+            eprintln!("ERROR: Command handle failed: {e}")
+        }
 
         buf.clear();
     }
