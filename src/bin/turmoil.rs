41d6baecf57188d0869f1458d525de0c6b6519fd
Subhan <s.ashraf9@protonmail.com>
2023-10-01 15:59:08 +0100

    remove turmoil

--- src/bin/turmoil.rs
+++ src/bin/turmoil.rs
@@ -1,64 +1,10 @@
-use std::{collections::HashMap, io, net::SocketAddr, ops::Range, sync::Arc, time::Duration};
+use std::{collections::HashMap, io, ops::Range, time::Duration};
 
-use hash_db::{
-    serverv2::{connection::Connection, message::Message},
-    storagev2::{
-        disk::Disk,
-        key_dir::{self, KeyDir},
-        page_manager::PageCache,
-        test::CleanUp,
-    },
-};
+use hash_db::storagev2::test::CleanUp;
 use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt, BufReader, BufWriter},
-    sync::RwLock,
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::ToSocketAddrs,
 };
-use turmoil::{
-    net::{TcpListener, TcpStream},
-    Builder,
-};
-
-macro_rules! clients {
-    ( $sim:ident, [$( ($client:expr, $range:expr) ),*] ) => {
-        $(
-            $sim.client($client, async {
-                let mut socket = TcpStream::connect(("server", 4444)).await?;
-
-                let inserts = generate_inserts($range);
-                let mut buf = [0; 256];
-                for (k, v) in &inserts {
-                    socket
-                        .write(format!("insert {} {}\n", k, v).as_bytes())
-                        .await?;
-                    socket.flush().await?;
-
-                    let n = socket.read(&mut buf).await?;
-
-                    assert!(&buf[0..n] == b"Success\n");
-                }
-
-                for (k, v) in &inserts {
-                    socket.write(format!("get {}\n", k).as_bytes()).await?;
-                    socket.flush().await?;
-
-                    let n = socket.read(&mut buf).await?;
-
-                    let exp = format!("{} {}\n", k, v);
-                    let got = &buf[0..n];
-
-                    assert!(
-                        got == exp.as_bytes(),
-                        "\nExpected: {}\nGot: {}\n",
-                        exp,
-                        std::str::from_utf8(got).unwrap()
-                    );
-                }
-
-                Ok(())
-            });
-        )*
-    };
-}
 
 fn generate_inserts(range: Range<u16>) -> HashMap<String, String> {
     let mut ret = HashMap::with_capacity(range.len());
@@ -75,85 +21,91 @@ fn generate_inserts(range: Range<u16>) -> HashMap<String, String> {
 
 const DB_FILE: &str = "main.db";
 
-// FIXME:
-// 1. Replacer is not replacing - LRUK nodes are initialised as unevictable but nothing sets them to
-//    be evictible. Causes database to lock up after filling read page buffer.
-// 2. Setting LRUK nodes to be evictable by default gets rid of the locking, but there is then an
-//    issue where entries are being written into pages they wouldn't fit. This maybe cause of
-//    asserts failing in the simulation.
-fn main() -> io::Result<()> {
-    let _cu = CleanUp::file(DB_FILE);
+struct Client<T: ToSocketAddrs> {
+    id: u8,
+    addr: T,
+    range: Range<u16>,
+}
 
-    let mut sim = Builder::new()
-        .simulation_duration(Duration::from_secs(240))
-        .build();
+impl<T: ToSocketAddrs> Client<T> {
+    pub fn new(id: u8, addr: T, range: Range<u16>) -> Self {
+        Self { id, addr, range }
+    }
 
-    sim.host("server", || async {
-        run().await;
+    pub async fn run(&self) -> io::Result<()> {
+        let mut socket = tokio::net::TcpStream::connect(&self.addr).await?;
+
+        let inserts = generate_inserts(self.range.clone());
+        let mut buf = [0; 256];
+        for (k, v) in &inserts {
+            socket
+                .write(format!("insert {} {}\n", k, v).as_bytes())
+                .await?;
+            socket.flush().await?;
+
+            let n = socket.read(&mut buf).await?;
+
+            assert!(&buf[0..n] == b"Success\n");
+        }
+
+        for (k, v) in &inserts {
+            socket.write(format!("get {}\n", k).as_bytes()).await?;
+            socket.flush().await?;
+
+            let n = socket.read(&mut buf).await?;
+
+            let exp = format!("{} {}\n", k, v);
+            let got = &buf[0..n];
+
+            assert!(
+                got == exp.as_bytes(),
+                "\nExpected: {}\nGot: {}\n",
+                exp,
+                std::str::from_utf8(got).unwrap()
+            );
+        }
+
+        eprintln!("{}: SUCCESS", self.id);
 
         Ok(())
+    }
+}
+
+#[tokio::main]
+pub async fn main() -> io::Result<()> {
+    let _cu = CleanUp::file(DB_FILE);
+
+    // TODO: add timeout
+    let sh = tokio::spawn(async {
+        hash_db::serverv2::server::run().await;
     });
 
-    clients!(sim, [("A", 0..1000), ("B", 1000..2000), ("C", 3000..4000)]);
+    tokio::time::sleep(Duration::from_secs(1)).await;
 
-    if let Err(e) = sim.run() {
-        eprintln!("ERROR: {}", e.to_string());
-    } else {
-        eprintln!("SUCCESS");
-    }
+    let ch0 = tokio::spawn(async {
+        let c = Client::new(0, "0.0.0.0:4444", 0..1000);
+        if let Err(e) = c.run().await {
+            eprintln!("{} failed with error: {}", c.id, e);
+        }
+    });
+
+    let ch1 = tokio::spawn(async {
+        let c = Client::new(1, "0.0.0.0:4444", 1000..2000);
+        if let Err(e) = c.run().await {
+            eprintln!("{} failed with error: {}", c.id, e);
+        }
+    });
+
+    let ch2 = tokio::spawn(async {
+        let c = Client::new(2, "0.0.0.0:4444", 3000..4000);
+        if let Err(e) = c.run().await {
+            eprintln!("{} failed with error: {}", c.id, e);
+        }
+    });
+
+    let _ = tokio::join!(sh, ch0, ch1, ch2);
+
+    eprintln!("FINISH");
 
     Ok(())
-}
-
-// Mostly the same, tcp listener/stream swapped out for turmoil and signal handler removed
-pub async fn run() {
-    let disk = Disk::new(DB_FILE).await.expect("Failed to open db file");
-    let (kd, latest, latest_id) = key_dir::bootstrap(&disk).await;
-    let kd = Arc::new(RwLock::new(kd));
-
-    let m = PageCache::new(disk, 2, latest, latest_id);
-
-    let listener = TcpListener::bind("0.0.0.0:4444")
-        .await
-        .expect("Could not bind");
-
-    loop {
-        match listener.accept().await {
-            Ok((stream, addr)) => {
-                tokio::spawn(accept(stream, addr, m.clone(), kd.clone()));
-            }
-            Err(e) => eprintln!("ERROR: {}", e),
-        }
-    }
-}
-
-async fn accept(stream: TcpStream, addr: SocketAddr, pc: PageCache, kd: Arc<RwLock<KeyDir>>) {
-    if let Err(e) = accept_loop(stream, addr, pc, kd).await {
-        eprintln!("ERROR: {}", e);
-    }
-}
-
-async fn accept_loop(
-    stream: TcpStream,
-    _addr: SocketAddr,
-    pc: PageCache,
-    kd: Arc<RwLock<KeyDir>>,
-) -> io::Result<()> {
-    let (reader, writer) = stream.into_split();
-    let reader = BufReader::new(reader);
-    let writer = BufWriter::new(writer);
-
-    let mut conn = Connection::new(reader, writer);
-
-    loop {
-        let message = match conn.read().await? {
-            Some(m) if m == Message::None => continue,
-            Some(m) => m,
-            None => continue,
-        };
-
-        let res = message.exec(&pc, &kd).await;
-
-        conn.write(res).await?;
-    }
 }
